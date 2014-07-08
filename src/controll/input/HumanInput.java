b652cb5c141b30b24783f204f772946172deecfd
Lord-Erik <erik.thunig@gmail.com>
2014-07-08 14:43:36 +0200

    HumanInpuz.java penetration

--- src/controll/input/HumanInput.java
+++ src/controll/input/HumanInput.java
@@ -10,8 +10,22 @@ public class HumanInput extends KeyAdapter {
     @Override
     public void keyPressed(KeyEvent e) {
         super.keyPressed(e);
+        
         if(e.getKeyCode()==KeyEvent.VK_A||e.getKeyCode()==KeyEvent.VK_LEFT){
             System.out.println("Left");
+            
+            if(e.getKeyCode()==KeyEvent.VK_A||e.getKeyCode()==KeyEvent.VK_RIGHT){
+                System.out.println("Right");
+            
+                if(e.getKeyCode()==KeyEvent.VK_A||e.getKeyCode()==KeyEvent.VK_UP){
+                    System.out.println("Up");
+                    
+                    if(e.getKeyCode()==KeyEvent.VK_A||e.getKeyCode()==KeyEvent.VK_DOWN){
+                        System.out.println("Down");                  
+                
+                    }
+                }  
+            }
         }
     }
     
